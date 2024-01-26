1bbae219439f3827c505c68f94346b2d060e4190
tarcio teixeia <tarciogbteixeira@gmail.com>
2024-01-26 10:02:37 -0200

    indentificando erro

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,9 @@
+AOS.init();
 
-
-const dataDoEvento = new Date("Jan 16, 2024 00:00:00");
+const dataDoEvento = new Date("Dec 12, 2022 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
-const contaAsHoras = setInterval(function () {
+const contaAsHoras = setInterval(function() {
     const agora = new Date();
     const timeStampAtual = agora.getTime();
 
@@ -15,9 +15,13 @@ const contaAsHoras = setInterval(function () {
 
     const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
     const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
-    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horaEmMs / minutoEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
     const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
 
     document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
-}, 1000);
 
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000);
