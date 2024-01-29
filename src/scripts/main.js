75c2d8ececb1d534d466642f95976db4462b1e11
tarcio teixeia <tarciogbteixeira@gmail.com>
2024-01-29 11:17:07 -0200

    finalizando styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 12, 2022 19:00:00");
+const dataDoEvento = new Date("Jan 16, 2025 00:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
